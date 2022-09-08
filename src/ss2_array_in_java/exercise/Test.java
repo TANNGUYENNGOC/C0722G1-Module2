6fdde28839324c429658e5583c27f43175f0f6ea
TANNGUYENNGOC <nguyentantai1610@gmail.com>
2022-09-09 00:28:15 +0700

    update bai tap ss3

--- src/ss2_array_in_java/exercise/Test.java
+++ src/ss2_array_in_java/exercise/Test.java
@@ -4,18 +4,5 @@ import java.util.Scanner;
 public class Test {
     public static void main(String[] args) {
 
-    int[] arrNum = new int[]{1,2,3,4,5};
-    thayDoi(arrNum);
-    System.out.println(Arrays.toString(arrNum));
-
-    int x = 10;
-    thayDoi1(x);
-    System.out.println(x);
-    }
-    static void thayDoi(int[]arr){
-        arr[0] = 100;
-    }
-    static void thayDoi1(int n){
-        n = 20;
     }
 }
