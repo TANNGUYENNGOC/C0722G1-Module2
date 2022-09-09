e0e3f09a35eb2ff9e6a576537c1946ce6f7a5d49
TANNGUYENNGOC <nguyentantai1610@gmail.com>
2022-09-09 15:08:41 +0700

    update bai tap ss4

--- src/ss4_class_and_object/exercises/exercise_3/Main.java
+++ src/ss4_class_and_object/exercises/exercise_3/Main.java
@@ -5,8 +5,18 @@ public class Main {
         boolean on = true;
         boolean off = false;
         Fan fan1 = new Fan();
+        fan1.setSpeed(3);
+        fan1.setRadius(10);
+        fan1.setColor("yellow");
+        fan1.setOn(true);
         System.out.println(fan1);
-        fan1.setOn(on);
-        System.out.println(fan1);
+
+        Fan fan2 = new Fan();
+        fan2.setSpeed(2);
+        fan2.setRadius(5);
+        fan2.setColor("blue");
+        fan2.setOn(false);
+        System.out.println(fan2);
+
     }
 }
